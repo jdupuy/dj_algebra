169a1dcce4307fc923331f8086559f71796205b0
Jonathan Dupuy <etu.jdupuy@gmail.com>
2018-05-29 12:25:25 +0200

    typo

--- dj_algebra.h
+++ dj_algebra.h
@@ -844,7 +844,7 @@ mat4::homogeneous::orthographic(
 	float_t bottom, float_t top,
 	float_t zNear, float_t zFar
 ) {
-	DJA_ASSERT(left != right && bottom != top && near != far);
+	DJA_ASSERT(left != right && bottom != top && zNear != zFar);
 	float_t c1 = float_t(1) / (right - left);
 	float_t c2 = float_t(1) / (top - bottom);
 	float_t c3 = float_t(1) / (zFar - zNear);
